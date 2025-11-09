4a4963ea162d819ccec2cdd25a8f9345fdbbec57
rapeepat <rapeepattiamklee@gmail.com>
2025-11-09 10:19:17 +0700

    Fig All Done

--- scripts/changelanguage.js
+++ scripts/changelanguage.js
@@ -1,26 +1,25 @@
- // โหลดภาษา (default = th)
-  let currentLang = localStorage.getItem('lang') || 'th';
-  loadLanguage(currentLang);
-console.log(currentLang)
-  // เปลี่ยนภาษาเมื่อคลิก
-  document.querySelectorAll('.lang-btn').forEach(btn => {
-    btn.addEventListener('click', (e) => {
-      e.preventDefault();
-      const lang = btn.dataset.lang;
-      localStorage.setItem('lang', lang);
-      loadLanguage(lang);
-    });
+// โหลดภาษา (default = th)
+let currentLang = localStorage.getItem('lang') || 'en';
+loadLanguage(currentLang);
+// เปลี่ยนภาษาเมื่อคลิก
+document.querySelectorAll('.lang-btn').forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const lang = btn.dataset.lang;
+    localStorage.setItem('lang', lang);
+    loadLanguage(lang);
   });
+});
 
-  function loadLanguage(lang) {
-    fetch(`./scripts/language/${lang}.json`)
-      .then(res => res.json())
-      .then(data => {
-        // เปลี่ยนข้อความในทุก element ที่มี data-key
-        document.querySelectorAll('[data-key]').forEach(el => {
-          const key = el.getAttribute('data-key');
-          const text = key.split('.').reduce((obj, i) => obj?.[i], data);
-          if (text) el.textContent = text;
-        });
+function loadLanguage(lang) {
+  fetch(`./scripts/language/${lang}.json`)
+    .then(res => res.json())
+    .then(data => {
+      // เปลี่ยนข้อความในทุก element ที่มี data-key
+      document.querySelectorAll('[data-key]').forEach(el => {
+        const key = el.getAttribute('data-key');
+        const text = key.split('.').reduce((obj, i) => obj?.[i], data);
+        if (text) el.textContent = text;
       });
-  }
+    });
+}
